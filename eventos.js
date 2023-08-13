d6b14659f30cff509a602e6d9d904fd928ae012f
ferbermu <f3rbermudez@gmail.com>
2023-08-13 16:16:32 -0300

    solucion-click
    solucionado el problema de las alertas

--- eventos.js
+++ eventos.js
@@ -1,7 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
     let miDiv = document.getElementById("miDiv");
+    let botonSaludar = document.getElementById("botonSaludar");
 
-    miDiv.addEventListener("click", function() {
+    miDiv.addEventListener("click", function(event) {
         alert("Hola! Soy el div");
     });
+
+    botonSaludar.addEventListener("click", function(event) {
+        event.stopPropagation(); 
+        alert("Hola!");
+    });
 });
+
